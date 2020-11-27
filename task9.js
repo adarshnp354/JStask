46630e1ae8b134e2af690584994baa14b7b732a3
adarsh354 <fullstack_trainee_001@thefuturalabs.com>
2020-11-27 17:27:27 +0530

    Update task9.js

--- task9.js
+++ task9.js
@@ -1,9 +1,11 @@
 var a=["J","A","V","A","S","C","R","I","P","T"]
 var b=[]
 var c
-for(var i=1;i<=10;i++){
-    for(c=1;c<=i;c++){
+var d=[]
+for(var i=0;i<=10;i++){
+    for(c=0;c<=i;c++){
         b=(a[i])
     }
-    console.log(b)
+    d=d+b
+    console.log(d)
 }
