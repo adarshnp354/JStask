e702c0fad73c2008a92ef1981e72772f45260d49
adarsh354 <fullstack_trainee_001@thefuturalabs.com>
2020-11-27 13:06:20 +0530

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,14 +1,17 @@
-function prime(){
-    var n,i,flag=true
-    n=document.a.n.value
-    n=parseInt(n)
-    for(i = 2; i <= n - 1; i++ )
-        if(n % i == 0){
-            flag = false;
-            break;
+function prime(n){
+    if(n===1){
+        return false
+    }
+    else if(n===2){
+        return true
+    }
+    else{
+        for(var x=2;x<n;x++){
+            if(n%x===0){
+                return false
+            }
         }
-    if(flag == true)
-        console.log(i + "is prime")
-    else
-        console.log(i + "not prime")
+        return true
+    }
 }
+console.log(prime(4))
